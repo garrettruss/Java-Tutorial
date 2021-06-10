7ad66295ae52b9b2098d92c1edbc131e370659b7
garrettruss <garrettmruss@gmail.com>
2021-06-10 12:43:43 -0400

    save

--- HelloWorld.java
+++ HelloWorld.java
@@ -114,6 +114,21 @@ Semicolons mark the end of a statement.
 Java is a compiled language.
 Compiling catches mistakes in our code.
 Compilers transform code into an executable class.
+
+Java is a case-sensitive language. Case sensitivity means that syntax, the words our computer understands, must match the case. For example, the Java command for outputting text to the screen is System.out.println(). If you were to type system.out.println() or System.Out.println(), the compiler would not know that your intention was to use System or out.
+
+All Java programs are made of at least one class. The class name must match the file: our file is HelloWorld.java and our class is HelloWorld. We capitalize every word, a style known as pascal case. Java variables and methods are named in a similar style called camel case where every word after the first is capitalized.
+
+The curly braces, { and }, mark the scope of the class. Everything inside the curly braces is part of the class.
+
+Every Java program must have a method called main(). A method is a sequence of tasks for the computer to execute. This main() method holds all of the instructions for our program.
+
+System.out.println("Hello World!");
+This code uses the method println() to send the text “Hello World!” to the terminal as output. println() comes from an object called out, which is responsible for various types of output. Objects are packages of state and behavior, and they’re often modeled on real-world things.
+
+out is located within System, which is another object responsible for representing our computer within the program! We can access parts of an object with a ., which is known as dot notation.
+
+This line of code is a statement, because it performs a single task. Statements always conclude with a semicolon.
  */
 
  public class HelloWorld {
