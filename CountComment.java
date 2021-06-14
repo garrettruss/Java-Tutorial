1a7fe76c81e29a912b8b3a21a1869100b4f6a6eb
garrettruss <garrettmruss@gmail.com>
2021-06-14 16:07:41 -0400

    save

---
+++ CountComment.java
@@ -0,0 +1,14 @@
+//This is the class declaration
+public class CountComment {
+    // This is the main method that runs when you compile
+    public static void main(String[] args) {
+        // This is where you will define your variable
+
+        // This is where you will print your variable
+
+    }
+
+    // This is the end of the class
+}
+
+// This is outside the class
