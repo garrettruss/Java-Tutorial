97cafcf25d1310095447bf530ba5486a3bfffb8a
garrettruss <garrettmruss@gmail.com>
2021-06-15 09:54:32 -0400

    save

---
+++ BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+    public static void main(String[] args) {
+        double balance = 1000.75;
+        double amountToWithdraw = 250;
+        double updatedBalance = balance - amountToWithdraw;
+        double amountForEachFriend = updatedBalance / 3;
+        boolean canPurchaseTicket = amountForEachFriend >= 250;
+        System.out.println(canPurchaseTicket);
+        String amount = "I gave each friend " + amountForEachFriend + "...";
+        System.out.println(amount);
+    }
+}
---
+++ Zoo.java
@@ -0,0 +1,22 @@
+/*
+The + operator, which we used for adding numbers together, can be used to concatenate Strings. In other words, we can use it to join two Strings together!
+
+We can even use a primitive datatype as the second variable to concatenate, and Java will intelligently make it a String first:
+
+int balance = 10000;
+String message = "Your balance is: " + balance;
+System.out.println(message);
+This code will print:
+
+Your balance is: 10000
+*/
+
+public class Zoo {
+    public static void main(String[] args) {
+        int animals = 12;
+        String species = "zebra";
+        String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+        System.out.println(zooDescription);
+
+    }
+}
