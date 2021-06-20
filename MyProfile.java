3bcc01c65dcab07d8112a777ec19c98ff5fee3b7
garrettruss <garrettmruss@gmail.com>
2021-06-20 14:24:34 -0400

    save

--- MyProfile.java
+++ MyProfile.java
@@ -5,7 +5,7 @@ boolean, which stores true and false.
 char, which stores single characters using single quotes.
 String, which stores multiple characters using double quotes.
 Static typing, which is one of the safety features of Java.
-Variable naming conventions.
+Variable naming conventions.\
 */
 
 public class MyProfile {
