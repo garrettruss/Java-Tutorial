18385d7a63967461963a530f447f6775fdb7e323
garrettruss <garrettmruss@gmail.com>
2021-07-15 13:22:53 -0400

    save

--- Store.java
+++ Store.java
@@ -1,27 +1,23 @@
 public class Store {
     // instance fields
     String productType;
-    int inventoryCount;
-    double inventoryPrice;
+    double price;
 
-    // In order to create an object (an instance of a class), we need a constructor
-    // method. The constructor is defined within the class. A method is a task that
-    // an object of a class performs
     // constructor method
-    public Store(String product, int count, double price) {
+    public Store(String product, double initialPrice) {
         productType = product;
-        inventoryCount = count;
-        inventoryPrice = price;
+        price = initialPrice;
+    }
+
+    // increase price method
+    public void increasePrice(double priceToAdd) {
+        double newPrice = price + priceToAdd;
+        price = newPrice;
     }
 
     // main method
     public static void main(String[] args) {
-        Store lemonadeStand = new Store("lemonade", 42, .99);
-        Store cookieShop = new Store("cookies", 12, 3.75);
-
-        System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice
-                + " per unit.");
-
-        System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
+        Store lemonadeStand = new Store("Lemonade", 3.75);
+        lemonadeStand.increasePrice(1.5);
     }
 }
