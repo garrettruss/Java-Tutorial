79f2fa18baa95fd3620b59026e6c271921387771
garrettruss <garrettmruss@gmail.com>
2021-07-31 14:45:38 -0400

    save

---
+++ CheckForJacket.java
@@ -0,0 +1,17 @@
+public class CheckForJacket {
+public static boolean checkForJacket(String[] lst) {
+  for (int i = 0; i < lst.length; i++) {
+    System.out.println(lst[i]);
+    if (lst[i] == "jacket") {
+      return true;
+    }
+  }
+  return false;
+} 
+ 
+public static void main(String[] args) {
+  String[] suitcase = {"shirt", "jacket", "pants", "socks"};   
+  System.out.println(checkForJacket(suitcase));
+}
+    
+}
--- Coffee.java
+++ Coffee.java
@@ -10,6 +10,14 @@ class Coffee {
             cupsOfCoffee++;
         }
 
+        /*
+         * for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+         * 
+         * System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+         * 
+         * }
+         */
+
     }
 
 }
