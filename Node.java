8ccff8bb142c654d83b255a9cf003e609adaefbc
garrettruss <garrettmruss@gmail.com>
2021-08-04 19:09:34 -0400

    save

--- Node.java
+++ Node.java
@@ -1,15 +1,26 @@
 public class Node {
+
     public String data;
-    public Node next;
+    private Node next;
 
     public Node(String data) {
         this.data = data;
         this.next = null;
     }
 
-    public static void main(String[] args) {
-        Node firstNode = new Node("I am a Node!");
-        System.out.println(firstNode.data);
-        System.out.println(firstNode.next);
+    public void setNextNode(Node node) {
+        this.next = node;
     }
+
+    public Node getNextNode() {
+        return this.next;
+    }
+
+    public static void main(String[] args) {
+        Node firstNode = new Node("I am the first Node!");
+        Node secondNode = new Node("I am the second Node!");
+        firstNode.setNextNode(secondNode);
+        System.out.println(firstNode.getNextNode().data);
+    }
+
 }
