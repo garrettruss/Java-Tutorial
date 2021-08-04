fd7820de347a91b123f43c89e85e8ac886f3e993
garrettruss <garrettmruss@gmail.com>
2021-08-04 09:35:54 -0400

    save

--- Bank.java
+++ Bank.java
@@ -3,15 +3,14 @@ public class Bank {
     private CheckingAccount accountTwo;
 
     public Bank() {
-        accountOne = new CheckingAccount("Zeus", 100, "1");
-        accountTwo = new CheckingAccount("Hades", 200, "2");
+        this.accountOne = new CheckingAccount("Zeus", 100, "1");
+        this.accountTwo = new CheckingAccount("Hades", 200, "2");
     }
 
     public static void main(String[] args) {
         Bank bankOfGods = new Bank();
-        System.out.println(bankOfGods.accountOne.getBalance());
-        bankOfGods.accountOne.setBalance(5000);
-        System.out.println(bankOfGods.accountOne.getBalance());
+        bankOfGods.accountOne.getAccountInformation();
+        //bankOfGods.accountOne.calculateNextMonthInterest();
     }
 
 }
--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,21 +1,29 @@
-public class CheckingAccount {
-    private String name;
-    private int balance;
-    private String id;
+public class CheckingAccount{
+  private String name;
+  private int balance;
+  private String id;
+  private double interestRate;
 
-    public CheckingAccount(String inputName, int inputBalance, String inputId) {
-        name = inputName;
-        balance = inputBalance;
-        id = inputId;
-    }
+  public CheckingAccount(String inputName, int inputBalance, String inputId){
+    this.name = inputName;
+    this.balance = inputBalance;
+    this.id = inputId;
+    this.interestRate = 0.02;
+  }
 
-    // Write new methods here:
-    public int getBalance() {
-        return balance;
-    }
+  public void getAccountInformation(){
+    System.out.println("Money in account: " + this.getBalance());
+    System.out.println("Next Month's Interest: " + this.calculateNextMonthInterest());
 
-    public void setBalance(int newBalance) {
-        balance = newBalance;
-    }
+  }
+
+  private int getBalance(){
+    return this.balance;
+  }
+
+  // Write the calculateNextMonthInterest() here
+  private double calculateNextMonthInterest(){
+    return this.interestRate * this.balance;
+  }
 
 }
