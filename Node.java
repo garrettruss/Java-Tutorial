c142140b999de938f945234032d2f0454b143c0e
garrettruss <garrettmruss@gmail.com>
2021-08-04 19:01:17 -0400

    save

---
+++ Node.java
@@ -0,0 +1,15 @@
+public class Node {
+    public String data;
+    public Node next;
+
+    public Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public static void main(String[] args) {
+        Node firstNode = new Node("I am a Node!");
+        System.out.println(firstNode.data);
+        System.out.println(firstNode.next);
+    }
+}
