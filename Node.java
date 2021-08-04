d997e12c8de57af7ff529c6db58cc89394698f71
garrettruss <garrettmruss@gmail.com>
2021-08-04 19:26:17 -0400

    save

--- Node.java
+++ Node.java
@@ -17,10 +17,20 @@ public class Node {
     }
 
     public static void main(String[] args) {
-        Node firstNode = new Node("I am the first Node!");
-        Node secondNode = new Node("I am the second Node!");
-        firstNode.setNextNode(secondNode);
-        System.out.println(firstNode.getNextNode().data);
+        Node strawberry = new Node("Berry Tasty");
+        Node banana = new Node("Banana-rama");
+        Node coconut = new Node("Nuts for Coconut");
+
+        strawberry.setNextNode(banana);
+        banana.setNextNode(coconut);
+
+        Node currentNode = strawberry;
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.getNextNode();
+
+        }
+
     }
 
 }
