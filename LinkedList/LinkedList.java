bf104c9eb26ad1835d9b9bfb62cb24e2456c699e
garrettruss <garrettmruss@gmail.com>
2021-11-05 15:02:16 -0400

    save

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -38,6 +38,18 @@ public class LinkedList {
         return removedHead.data;
     }
 
+    public String printList() {
+        String output = "<head> ";
+        Node currentNode = this.head;
+        while (currentNode != null) {
+            output += currentNode.data + " ";
+            currentNode = currentNode.getNextNode();
+        }
+        output += "<tail>";
+        System.out.println(output);
+        return output;
+    }
+
     public static void main(String[] args) {
 
     }
