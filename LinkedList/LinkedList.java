543a026494d2d968e951b2b844a391d41d481fe6
garrettruss <garrettmruss@gmail.com>
2021-11-05 14:38:07 -0400

    save

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+public class LinkedList {
+
+    public Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void addToHead(String data) {
+        Node newHead = new Node(data);
+        Node currentHead = this.head;
+        this.head = newHead;
+        if (currentHead != null) {
+            this.head.setNextNode(currentHead);
+        }
+    }
+
+    public void addToTail(String data) {
+        Node tail = this.head;
+        if (tail == null) {
+            this.head = new Node(data);
+        } else {
+            while (tail.getNextNode() != null) {
+                tail = tail.getNextNode();
+            }
+            tail.setNextNode(new Node(data));
+        }
+    }
+
+    public String removeHead() {
+        Node removedHead = this.head;
+        if (removedHead == null) {
+            return null;
+        }
+        this.head = removedHead.getNextNode();
+        return removedHead.data;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
