a3bce0ecae61cebb4becf83dbffa860deb05b90b
garrettruss <garrettmruss@gmail.com>
2021-11-09 15:17:50 -0500

    asymptotic notation

---
+++ Recursion/FactorialThree.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+public class FactorialThree {
+    public static int recursiveFactorial(int n) {
+        if (n > 0) {
+            return n * recursiveFactorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+
+    public static int iterativeFactorial(int n) {
+        int result = 1;
+
+        while (n > 0) {
+            result *= n;
+            n -= 1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int recursiveSolution = recursiveFactorial(10);
+        System.out.println("The recursive solution to 10! is: " + recursiveSolution);
+
+        int iterativeSolution = iterativeFactorial(10);
+        System.out.println("The iterative solution to 10! is: " + iterativeSolution);
+    }
+}
---
+++ Recursion/Node.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Node {
+    public String data;
+    private Node next;
+
+    public Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public void setNextNode(Node node) {
+        this.next = node;
+    }
+
+    public Node getNextNode() {
+        return this.next;
+    }
+
+}
