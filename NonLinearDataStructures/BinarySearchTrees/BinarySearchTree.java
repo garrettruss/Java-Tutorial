15f49c316b533da47d5cfebab09d5e0f4e31a0b5
garrettruss <garrettmruss@gmail.com>
2021-11-16 09:44:03 -0500

    binary search tree

--- NonLinearDataStructures/BinarySearchTrees/BinarySearchTree.java
+++ NonLinearDataStructures/BinarySearchTrees/BinarySearchTree.java
@@ -125,27 +125,77 @@ public class BinarySearchTree {
         return null;
     }
 
+    /*
+     * There are two main ways of traversing a binary tree: breadth-first and
+     * depth-first. With breadth-first traversal, we begin traversing at the top of
+     * the tree’s root node, displaying its value and continuing the process with
+     * the left child node and the right child node. Descend a level and repeat this
+     * step until we finish displaying all the child nodes at the deepest level from
+     * left to right.
+     * 
+     * With depth-first traversal, we always traverse down each left-side branch of
+     * a tree fully before proceeding down the right branch. However, there are
+     * three traversal options:
+     * 
+     * Preorder is when we perform an action on the current node first, followed by
+     * its left child node and its right child node Inorder is when we perform an
+     * action on the left child node first, followed by the current node and the
+     * right child node Postorder is when we perform an action on the left child
+     * node first, followed by the right child node and then the current node
+     * 
+     * For this lesson, we will implement the inorder option. The advantage of this
+     * option is that the value is displayed in sorted order from the smallest to
+     * the biggest.
+     * 
+     * To illustrate, let’s say we have a binary tree that looks like this:
+     * 
+     * 15 /------+-----\ 12 20 / \ / \ 10 13 18 22 / \ / \ / \ / \ 8 11 12 14 16 19
+     * 21 25 We begin by traversing the left subtree at each level, which brings us
+     * to 8, 10, and 11 reside. The inorder traversal would be:
+     * 
+     * 8, 10, 11 We ascend one level up to visit root node 12 before we descend back
+     * to the bottom where the right subtree of 12, 13, and 14 resides. Inorder
+     * traversal continues with:
+     * 
+     * 12, 12, 13, 14 We again ascend one level up to visit root node 15 before we
+     * traverse the right subtree starting at the bottom level again. We continue
+     * with the bottom leftmost subtree where 16, 18, and 19 reside. The inorder
+     * traversal continues with:
+     * 
+     * 15, 16, 18, 19 We ascend one level up to visit root node 20 before we descend
+     * back to the bottom where the rightmost subtree of 21, 22, and 25 resides.
+     * 
+     * Traversal finishes with:
+     * 
+     * 20, 21, 22, 25 The entire traversal becomes:
+     * 
+     * 8, 10, 11, 12, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 25 Notice that all the
+     * values displayed are sorted in ascending order.
+     * 
+     * Let’s implement a method that prints the inorder traversal for the
+     * BinarySearchTree class!
+     */
+        public void depthFirstTraversal() {
+        if (this.left != null) {
+            this.left.depthFirstTraversal();
+        }
+        System.out.println(this.value);
+        if (this.right != null) {
+            this.right.depthFirstTraversal();
+        }
+     }
+
     public static void main(String[] args) {
-        // Create a new BST:
-        BinarySearchTree root = new BinarySearchTree(100);
+        BinarySearchTree tree = new BinarySearchTree(48);
+        tree.insert(24);
+        tree.insert(55);
+        tree.insert(26);
+        tree.insert(38);
+        tree.insert(56);
+        tree.insert(74);
 
-        // Insert values below:
-        root.insert(50);
-        root.insert(125);
-        root.insert(75);
-        root.insert(25);
-
-        /*
-            100
-            /   \
-        50    125
-        /  \
-        25   75ß
-        */
-
-        // Get nodes by value below
-        System.out.println(root.getNodeByValue(75)); 
-        System.out.println(root.getNodeByValue(55));
+        // Print depth-first traversal
+        tree.depthFirstTraversal();
 
     }
 }
