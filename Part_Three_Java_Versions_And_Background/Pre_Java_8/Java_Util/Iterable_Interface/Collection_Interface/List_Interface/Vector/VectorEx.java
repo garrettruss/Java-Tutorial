5d1c40a8ebf370db75d7714fc6fc042284f0b90a
garrettruss <garrettmruss@gmail.com>
2022-02-03 21:22:35 -0500

    save

--- Part_Three_Java_Versions_And_Background/Pre_Java_8/Java_Util/Iterable_Interface/Collection_Interface/List_Interface/Vector/VectorEx.java
+++ Part_Three_Java_Versions_And_Background/Pre_Java_8/Java_Util/Iterable_Interface/Collection_Interface/List_Interface/Vector/VectorEx.java
@@ -1,10 +1,43 @@
 package Part_Three_Java_Versions_And_Background.Pre_Java_8.Java_Util.Iterable_Interface.Collection_Interface.List_Interface.Vector;
 
 /*
-Vector uses a dynamic array to store the data elements. It is similar to ArrayList. However, It is synchronized and contains many methods that are not the part of Collection framework.
+
+
+A vector provides us with dynamic arrays in Java. Though, it may be slower than standard arrays but can be helpful in programs where lots of manipulation in the array is needed. This is identical to ArrayList in terms of implementation. However, the primary difference between a vector and an ArrayList is that a Vector is synchronized and an ArrayList is non-synchronized.
 */
 
 
-public class VectorEx {
-    
+
+// Java program to demonstrate the
+// working of Vector
+import java.io.*;
+import java.util.*;
+
+class VectorEx {
+
+    // Main Method
+    public static void main(String[] args) {
+
+        // Declaring the Vector
+        Vector<Integer> v = new Vector<Integer>();
+
+        // Appending new elements at
+        // the end of the list
+        for (int i = 1; i <= 5; i++)
+            v.add(i);
+
+        // Printing elements
+        System.out.println(v);
+
+        // Remove element at index 3
+        v.remove(3);
+
+        // Displaying the Vector
+        // after deletion
+        System.out.println(v);
+
+        // Printing elements one by one
+        for (int i = 0; i < v.size(); i++)
+            System.out.print(v.get(i) + " ");
+    }
 }
---
+++ Part_Three_Java_Versions_And_Background/Pre_Java_8/Java_Util/Iterable_Interface/Collection_Interface/Set_Interface/Sorted_Set_Interface/Navigable_Set_Interface/TreeSet/Treeset.java
@@ -0,0 +1,48 @@
+package Part_Three_Java_Versions_And_Background.Pre_Java_8.Java_Util.Iterable_Interface.Collection_Interface.Set_Interface.Sorted_Set_Interface.Navigable_Set_Interface.TreeSet;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Iterator;
+
+
+
+/* 
+
+https://www.geeksforgeeks.org/sortedset-java-examples/
+
+TreeSet: TreeSet class which is implemented in the collections framework is an implementation of the SortedSet Interface and SortedSet extends Set Interface. It behaves like a simple set with the exception that it stores elements in a sorted format. TreeSet uses a tree data structure for storage. Objects are stored in sorted, ascending order. But we can iterate in descending order using method TreeSet.descendingIterator(). Let’s see how to create a sortedset object using this class.
+
+*/
+
+public class Treeset {
+
+    public static void main(String[] args) {
+        SortedSet<String> ts = new TreeSet<String>();
+
+        // Adding elements into the TreeSet
+        // using add()
+        ts.add("India");
+        ts.add("Australia");
+        ts.add("South Africa");
+
+        // Adding the duplicate
+        // element
+        ts.add("India");
+
+        // Displaying the TreeSet
+        System.out.println(ts);
+
+        // Removing items from TreeSet
+        // using remove()
+        ts.remove("Australia");
+        System.out.println("Set after removing "
+                + "Australia:" + ts);
+
+        // Iterating over Tree set items
+        System.out.println("Iterating over set:");
+        Iterator<String> i = ts.iterator();
+        while (i.hasNext())
+            System.out.println(i.next());
+    }
+    
+}
